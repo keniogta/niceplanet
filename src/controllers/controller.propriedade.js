5479ffa2e31348e951a92423c522c76debbb841d
keniogta <kenio@gtasistemas.com.br>
2022-12-28 13:29:30 -0300

    excluir propriedade

--- src/controllers/controller.propriedade.js
+++ src/controllers/controller.propriedade.js
@@ -24,6 +24,18 @@ function Alterar(req, res){
     })
 }
 
+function Excluir(req, res){
+    modelPropriedade.Excluir(req.params.id_propriedade , (err, result) => {
+        if (err){
+            res.status(500).send(err)
+        } else if (result.length == 0){
+            res.status(401).json({erro:'Propriedade não encontrado.'})
+        } else{            
+            res.status(200).send('Propriedade excluída com sucesso.')
+        }
+    })
+}
+
 function ListarId(req, res){
     modelPropriedade.ListarId(req.params.id_propriedade, (err, result) => {
         if (err){
@@ -48,4 +60,4 @@ function ListarPorNome(req, res){
     })
 }
 
-export default {Inserir, Alterar, ListarId, ListarPorNome};
+export default {Inserir, Alterar, Excluir, ListarId, ListarPorNome};
--- src/routes/route.propriedade.js
+++ src/routes/route.propriedade.js
@@ -5,6 +5,7 @@ const routePropriedade = Router();
 
 routePropriedade.post('/v1/propriedades/cadastro',controllerPropriedade.Inserir);
 routePropriedade.put('/v1/propriedades/alterar',controllerPropriedade.Alterar);
+routePropriedade.post('/v1/propriedades/excluir/:id_propriedade',controllerPropriedade.Excluir);
 routePropriedade.get('/v1/propriedades/listar/:id_propriedade',controllerPropriedade.ListarId);
 routePropriedade.get('/v1/propriedades/nome',controllerPropriedade.ListarPorNome);
 
